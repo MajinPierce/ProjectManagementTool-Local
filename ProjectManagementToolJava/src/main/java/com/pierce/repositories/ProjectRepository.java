7fbec932346aedde88d4133cc5ac8d9ecd489bf6
PierceBeckett <pierceabeckett@gmail.com>
2022-01-18 17:01:42 -0500

    find project by projectId

--- ProjectManagementToolJava/src/main/java/com/pierce/repositories/ProjectRepository.java
+++ ProjectManagementToolJava/src/main/java/com/pierce/repositories/ProjectRepository.java
@@ -8,11 +8,8 @@ import com.pierce.domain.Project;
 @Repository
 public interface ProjectRepository extends CrudRepository<Project, Long>{
 
-	@Override
-	default Iterable<Project> findAllById(Iterable<Long> ids) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	
+	Project findByProjectIdentifier(String projectId);
 
 	
 }
