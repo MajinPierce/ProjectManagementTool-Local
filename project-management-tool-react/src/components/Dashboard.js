70485da2f2914a5718926f2706ede636009f9cd6
PierceBeckett <pierceabeckett@gmail.com>
2022-01-19 21:38:51 -0500

    project and header components

--- project-management-tool-react/src/components/Dashboard.js
+++ project-management-tool-react/src/components/Dashboard.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import ProjectItem from './Project/ProjectItem';
 
 class Dashboard extends Component {
   render() {
     return (
         <div>
             <h1>Welcome to the DASHBOARD</h1>
+            <ProjectItem />
+            <ProjectItem />
+            <ProjectItem />
         </div>
     );
   }
