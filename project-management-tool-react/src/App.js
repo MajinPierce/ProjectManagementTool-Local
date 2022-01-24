c862771107edef72df786e1782e6dae936186790
PierceBeckett <pierceabeckett@gmail.com>
2022-01-23 22:20:33 -0500

    finally added modern routing to this old project

--- project-management-tool-react/src/App.js
+++ project-management-tool-react/src/App.js
@@ -1,13 +1,18 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
-import Header from './components/Layout/Header';
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import React from "react";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
+import { Routes, Route } from "react-router-dom";
+import AddProject from "./components/Project/AddProject";
 
 function App() {
   return (
-    <div className='App'>
+    <div className="App">
       <Header />
-      <Dashboard />
+      <Routes>
+        <Route exact path="/dashboard" element={<Dashboard />} />
+        <Route exact path="/addProject" element={<AddProject />} />
+      </Routes>
     </div>
   );
 }
