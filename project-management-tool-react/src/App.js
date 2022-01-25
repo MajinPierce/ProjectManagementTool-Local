c4f6cb40ab55c56410aa6edd83644311c8212493
PierceBeckett <pierceabeckett@gmail.com>
2022-01-25 14:19:18 -0500

    downgrade router, refactor, update form works

--- project-management-tool-react/src/App.js
+++ project-management-tool-react/src/App.js
@@ -2,19 +2,23 @@ import "./App.css";
 import React from "react";
 import Dashboard from "./components/Dashboard";
 import Header from "./components/Layout/Header";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import AddProject from "./components/Project/AddProject";
 import UpdateProject from "./components/Project/UpdateProject";
+import { useLocation } from "react-router-dom";
 
 function App() {
+  const location = useLocation;
   return (
     <div className="App">
       <Header />
-      <Routes>
-        <Route exact path="/dashboard" element={<Dashboard />} />
-        <Route exact path="/addProject" element={<AddProject />} />
-        <Route exact path="/updateProject/:id" element={<UpdateProject />} />
-      </Routes>
+      <Router location={location} key={location.pathname}>
+        <Switch>
+          <Route exact path="/dashboard" component={Dashboard} />
+          <Route exact path="/addProject" component={AddProject} />
+          <Route exact path="/updateProject/:id" component={UpdateProject} />
+        </Switch>
+      </Router>
     </div>
   );
 }
--- project-management-tool-react/src/reducers/ProjectReducer.js
+++ project-management-tool-react/src/reducers/ProjectReducer.js
@@ -1,16 +1,22 @@
-import { GET_PROJECTS } from "../actions/Types";
+import { GET_PROJECT, GET_PROJECTS } from "../actions/Types";
 
 const initialState = {
   projects: [],
   project: {},
 };
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default (state = initialState, action) => {
   switch (action.type) {
     case GET_PROJECTS:
       return {
         ...state,
         projects: action.payload,
+      };
+    case GET_PROJECT:
+      return {
+        ...state,
+        project: action.payload,
       };
     default:
       return state;
