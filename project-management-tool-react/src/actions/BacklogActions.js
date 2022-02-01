5244f227af2d0cb48f2d865c21afce0236a0b412
PierceBeckett <pierceabeckett@gmail.com>
2022-02-01 12:32:36 -0500

    delete project task

--- project-management-tool-react/src/actions/BacklogActions.js
+++ project-management-tool-react/src/actions/BacklogActions.js
@@ -1,5 +1,10 @@
 import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./Types";
+import {
+  GET_ERRORS,
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK,
+} from "./Types";
 
 export const addProjectTask =
   (backlog_id, project_task, history) => async (dispatch) => {
@@ -62,3 +67,13 @@ export const updateProjectTask =
       });
     }
   };
+
+export const deleteProjectTask = (backlog_id, pt_id) => async (dispatch) => {
+  if (window.confirm("Delete it or jk?")) {
+    await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`);
+    dispatch({
+      type: DELETE_PROJECT_TASK,
+      payload: pt_id,
+    });
+  }
+};
