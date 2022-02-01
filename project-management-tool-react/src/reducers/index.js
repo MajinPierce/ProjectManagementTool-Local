88518c8aa26b1eb77f78590cea1394aa53c6b622
PierceBeckett <pierceabeckett@gmail.com>
2022-01-31 19:10:56 -0500

    create folders/files for backlog

--- project-management-tool-react/src/reducers/index.js
+++ project-management-tool-react/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
+import BacklogReducer from "./BacklogReducer";
 import ErrorReducer from "./ErrorReducer";
 import ProjectReducer from "./ProjectReducer";
 
 export default combineReducers({
   errors: ErrorReducer,
   project: ProjectReducer,
+  backlog: BacklogReducer,
 });
