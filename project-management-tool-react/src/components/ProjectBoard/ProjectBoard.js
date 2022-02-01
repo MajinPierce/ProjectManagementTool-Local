1aafaa88963f141b81f0699243c7273b8cd8d68f
PierceBeckett <pierceabeckett@gmail.com>
2022-01-31 22:44:44 -0500

    add project tasks to state

--- project-management-tool-react/src/components/ProjectBoard/ProjectBoard.js
+++ project-management-tool-react/src/components/ProjectBoard/ProjectBoard.js
@@ -1,13 +1,25 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import Backlog from "./Backlog";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import { getBacklog } from "../../actions/BacklogActions";
 
 class ProjectBoard extends Component {
-  render() {
+  //constructor to handle errors
+
+  componentDidMount() {
     const { id } = this.props.match.params;
+    this.props.getBacklog(id);
+  }
+
+  render() {
     return (
       <div className="container">
-        <Link to={`/addProjectTask/${id}`} className="btn btn-primary mb-3">
+        <Link
+          to={`/addProjectTask/${this.id}`}
+          className="btn btn-primary mb-3"
+        >
           <i className="fas fa-plus-circle"> Create Project Task</i>
         </Link>
         <br />
@@ -18,4 +30,13 @@ class ProjectBoard extends Component {
   }
 }
 
-export default ProjectBoard;
+ProjectBoard.propTypes = {
+  backlog: PropTypes.object.isRequired,
+  getBacklog: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  backlog: state.backlog,
+});
+
+export default connect(mapStateToProps, { getBacklog })(ProjectBoard);
