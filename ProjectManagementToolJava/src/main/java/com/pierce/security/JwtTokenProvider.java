65bd4f871c47260a3c1e6eff5256fcd51818f6d8
PierceBeckett <pierceabeckett@gmail.com>
2022-02-03 17:51:12 -0500

    jwt token can be used in api calls

--- ProjectManagementToolJava/src/main/java/com/pierce/security/JwtTokenProvider.java
+++ ProjectManagementToolJava/src/main/java/com/pierce/security/JwtTokenProvider.java
@@ -12,8 +12,7 @@ import static com.pierce.security.SecurityConstants.EXPIRATION_TIME;
 import static com.pierce.security.SecurityConstants.SECRET;
 import com.pierce.domain.User;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
 
 @Component
 public class JwtTokenProvider {
@@ -41,6 +40,29 @@ public class JwtTokenProvider {
         }
 	
 	//validate
+	public boolean validateToken(String token){
+        try{
+            Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token);
+            return true;
+        }catch (SignatureException ex){
+            System.out.println("Invalid JWT Signature");
+        }catch (MalformedJwtException ex){
+            System.out.println("Invalid JWT Token");
+        }catch (ExpiredJwtException ex){
+            System.out.println("Expired JWT token");
+        }catch (UnsupportedJwtException ex){
+            System.out.println("Unsupported JWT token");
+        }catch (IllegalArgumentException ex){
+            System.out.println("JWT claims string is empty");
+        }
+        return false;
+    }
 	
-	//user id -> token
+	//token -> userId
+	public Long getUserIdFromJWT(String token){
+        Claims claims = Jwts.parser().setSigningKey(SECRET).parseClaimsJws(token).getBody();
+        String id = (String)claims.get("id");
+
+        return Long.parseLong(id);
+    }
 }
