2505f383a537f1fd247e45ab79e24440bdbb5c2c
PierceBeckett <pierceabeckett@gmail.com>
2022-02-04 11:08:54 -0500

    project task date format

--- ProjectManagementToolJava/src/main/java/com/pierce/domain/ProjectTask.java
+++ ProjectManagementToolJava/src/main/java/com/pierce/domain/ProjectTask.java
@@ -15,6 +15,7 @@ import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.validation.constraints.NotBlank;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
@@ -30,6 +31,7 @@ public class ProjectTask {
     private String acceptanceCriteria;
     private String status;
     private Integer priority;
+    @JsonFormat(pattern="yyyy-mm-dd")
     private Date dueDate;
     
     // Many to One w/ Backlog
@@ -40,7 +42,10 @@ public class ProjectTask {
     
     @Column(updatable = false)
     private String projectIdentifier;
+    @JsonFormat(pattern="yyyy-mm-dd")
+	@Column(updatable=false)
     private Date createAt;
+    @JsonFormat(pattern="yyyy-mm-dd")
     private Date updateAt;
     
     
