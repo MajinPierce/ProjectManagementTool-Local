0f4c227793cdd7895a880df6eb401abc182e38e6
PierceBeckett <pierceabeckett@gmail.com>
2022-02-04 00:42:21 -0500

    security actions and reducers

--- project-management-tool-react/src/reducers/index.js
+++ project-management-tool-react/src/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from "redux";
 import BacklogReducer from "./BacklogReducer";
 import ErrorReducer from "./ErrorReducer";
 import ProjectReducer from "./ProjectReducer";
+import SecurityReducer from "./SecurityReducer";
 
 export default combineReducers({
   errors: ErrorReducer,
   project: ProjectReducer,
   backlog: BacklogReducer,
+  security: SecurityReducer,
 });
